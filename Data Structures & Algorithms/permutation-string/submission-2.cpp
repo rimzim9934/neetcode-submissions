d2a6cc0072bbe24e9af265f9340a664634fdb22a
rimzim9934 <rimjhimkum523@gmail.com>
2026-07-28 01:18:47 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+        if(n > m){
+            return false;
+        }
+        sort(s1.begin(),s1.end());
+        for(int i = 0;i<=m-n;i++){
+            string sub = s2.substr(i,n);
+        
+        sort(sub.begin(),sub.end());
+        if(sub == s1){
+            return true;
+        }
+        }
+        
+        return false;
+    }
+};
