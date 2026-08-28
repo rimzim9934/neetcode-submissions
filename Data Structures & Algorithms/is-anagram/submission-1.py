bbeb577356ca458f1f4a1d5b1b3ddf1f9d1b142d
rimzim9934 <rimjhimkum523@gmail.com>
2026-08-29 01:36:27 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hash_map = {}
+        for i in range(len(s)):
+            hash_map[s[i]] = hash_map.get(s[i], 0) + 1
+            hash_map[t[i]] = hash_map.get(t[i], 0) - 1
+            
+        return all(value == 0 for value in hash_map.values())
